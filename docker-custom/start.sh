22d6dcedb6509b99404daeb92ac12265f7304d6a
Gunnar <dagorwaith@gmail.com>
2020-11-12 12:54:05 +0100

    Update start.sh

--- docker-custom/start.sh
+++ docker-custom/start.sh
@@ -5,25 +5,25 @@
 #npm install node-red-contrib-noble-bluetooth
 
 # if spotify device name not defined in balena service var use host name
-if [ "$BLUETOOTH_DEVICE_NAME" == "none" ] ; then
-    BLUETOOTH_DEVICE_NAME=$BALENA_DEVICE_NAME_AT_INIT
-fi
+#if [ "$BLUETOOTH_DEVICE_NAME" == "none" ] ; then
+#    BLUETOOTH_DEVICE_NAME=$BALENA_DEVICE_NAME_AT_INIT
+#fi
 
 # set dbus address
 #export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 # configure bluetooth
-bluetoothctl power off
-bluetoothctl system-alias $BLUETOOTH_DEVICE_NAME
-bluetoothctl set-alias $BLUETOOTH_DEVICE_NAME
-bluetoothctl pairable on
-bluetoothctl discoverable on
-bluetoothctl discoverable-timeout 0
-bluetoothctl power on
+#bluetoothctl power off
+#bluetoothctl system-alias $BLUETOOTH_DEVICE_NAME
+#bluetoothctl set-alias $BLUETOOTH_DEVICE_NAME
+#bluetoothctl pairable on
+#bluetoothctl discoverable on
+#bluetoothctl discoverable-timeout 0
+#bluetoothctl power on
 
 # start dbus
-dbus-uuidgen --ensure
-dbus-daemon --system
+#dbus-uuidgen --ensure
+#dbus-daemon --system
 
 # allow node access to bt le
 setcap cap_net_raw+eip $(eval readlink -f $(which node))
